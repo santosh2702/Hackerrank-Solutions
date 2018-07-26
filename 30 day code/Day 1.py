ddd327fdeb87a4e9880221bd6d3517d0cb7dbdd4
sam2702 <Singh.santosh2702@gmail.com>
2018-07-26 23:55:30 +0530

    day 1

---
+++ 30 day code/Day 1.py
@@ -0,0 +1,14 @@
+# Declare second integer, double, and String variables.
+i1=int(input())
+d1=float(input())
+s1=input()
+
+
+print(i+i1)
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
