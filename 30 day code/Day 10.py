550c0dbaee7b9663dce86db329185a10ad9615d7
sam2702 <Singh.santosh2702@gmail.com>
2018-07-31 23:52:50 +0530

    commit

---
+++ 30 day code/Day 10.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+n = int(input().strip())
+
+binary = list(bin(n)[2:])
+#print (binary)
+
+count = 0
+max_count = 0
+for i in binary:
+    if (i == '1'):
+        count += 1
+    else:
+        if count > max_count:
+            max_count = count
+        count = 0
+if count > max_count:
+    max_count = count
+print (max_count)
