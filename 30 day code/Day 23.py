6d07fd94f18a7d2e1e27bef861ea776121c8dbb4
sam2702 <Singh.santosh2702@gmail.com>
2018-09-18 22:17:51 +0530

    changes

--- 30 day code/Day 23.py
+++ 30 day code/Day 23.py
@@ -1,23 +1,4 @@
-class Node:
-    def __init__(self, data):
-        self.right = self.left = None
-        self.data = data
-
-
-class Solution:
-    def insert(self, root, data):
-        if not root:
-            return Node(data)
-        else:
-            if data <= root.data:
-                current = self.insert(root.left, data)
-                root.left = current
-            else:
-                current = self.insert(root.right, data)
-                root.right = current
-        return root
-
-    def levelOrder(self, root):
+def levelOrder(self, root):
         queue = [root]
         while len(queue) is not 0:
             curr = queue[0]
@@ -28,10 +9,3 @@ class Solution:
                 queue.append(curr.left)
             if curr.right is not None:
                 queue.append(curr.right)
-T=int(input())
-myTree=Solution()
-root=None
-for i in range(T):
-    data=int(input())
-    root=myTree.insert(root,data)
-myTree.levelOrder(root)
